9a03ea0117f5eca8f21340882d21ccff26502108
CodeByAhmad <devdroid47@gmail.com>
2025-01-19 16:50:38 +0500

    Initial

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+
+  console.log("Hello")
+
+  return (
+    <View>
+      <Text>Social Clone</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
